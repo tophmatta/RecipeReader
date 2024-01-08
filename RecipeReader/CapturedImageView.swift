be19fa0390a3d46a13b0b7f5aa3eb9aedf57f017
Toph Matta <toph.matta@gmail.com>
2024-01-08 08:24:15 -0600

    Image nav working; better error handling

---
+++ RecipeReader/CapturedImageView.swift
@@ -0,0 +1,8 @@
+//
+//  CapturedImageView.swift
+//  RecipeReader
+//
+//  Created by Toph Matta on 1/5/24.
+//
+
+import Foundation
---
+++ RecipeReader/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  RecipeReader
+//
+//  Created by Toph Matta on 1/5/24.
+//
+
+import Foundation
